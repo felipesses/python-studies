c9470a0bf229a08858dec969b5849b77c66af05d
felipesses <felipeme73@gmail.com>
2020-12-24 20:37:37 -0300

    Implements introduction - Strings - Numbers - Variables

---
+++ introduction/variables/variables.py
@@ -0,0 +1,45 @@
+#######################################################
+
+# Calling variables
+# character_name = "Jason"
+# isMale = True
+# character_age = 50
+
+# print("There once \nwas a man named " + character_name)
+
+#######################################################
+
+# Strings
+
+# phrase = "Giraffe"
+
+# print(phrase)
+# print(phrase.upper())
+# print(phrase.lower())
+# print(phrase.isupper())  # False or True
+# print(len(phrase))  # How many characters are in the String
+# print(phrase[2])  # Get n character of String
+# print(phrase.replace("Giraffe", "Elephant"))
+# print(phrase.replace(phrase[0], "L"))
+
+#######################################################
+
+# Numbers
+
+# from math import *  # Import math module
+# my_num = 13
+
+# print(my_num)
+# print(str(my_num) + " is my favorite number")  # Convert number to string
+# print(abs(my_num))  # Absolute value
+# print(pow(4, 2))  # Power of number -> 4^2
+# print(min(4, 2))  # Minimun power
+# print(max(4, 2))  # Maximun power
+# print(round(20.34))  # Round number
+
+# # Math functions
+# print(floor(4))
+# print(ceil(3.7))
+# print(sqrt(36))
+
+#######################################################
