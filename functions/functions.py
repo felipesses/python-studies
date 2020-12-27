88714a5a78027b871053834f2e2c0ea76d588fed
felipesses <felipeme73@gmail.com>
2020-12-27 19:50:47 -0300

    Implements simple functions

---
+++ functions/functions.py
@@ -0,0 +1,19 @@
+def say_hi():
+    print("Hi!")
+
+
+say_hi()
+
+
+def sum_numbers(n1, n2):
+    print(n1 + n2)
+
+
+sum_numbers(2, 2)
+
+
+def say_name_age(name, age):
+    print("Hello, " + name + ", you are " + str(age) + " years old!")
+
+
+say_name_age("Felipe", 20)
