d950a08e41825e1509920c9f0be95633fd6eedad
felipesses <felipeme73@gmail.com>
2020-12-27 17:07:08 -0300

    Implements inputs and basic sum calculator

---
+++ introduction/inputs/calculator.py
@@ -0,0 +1,11 @@
+n1 = input("Enter a int number: ")
+n2 = input("Enter another int number: ")
+
+n3 = input("Enter a float number: ")
+n4 = input("Enter another float number: ")
+
+result_sum_int = int(n1) + int(n2)
+result_sum_float = float(n3) + float(n4)
+
+print(result_sum_int)
+print(result_sum_float)
